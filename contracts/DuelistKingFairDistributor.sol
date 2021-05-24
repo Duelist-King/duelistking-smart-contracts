6d8d2444ae6cc8b4b7be5c6f974250e5f23ea888
Chiro Hiro <chiro8x@gmail.com>
2021-05-25 01:42:57 +0700

    improve fair distribution with the divine

--- contracts/DuelistKingFairDistributor.sol
+++ contracts/DuelistKingFairDistributor.sol
@@ -5,34 +5,62 @@ pragma abicoder v2;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 import './DuelistKingCard.sol';
+import './DuelistKingItem.sol';
 import './DuelistKingRegistry.sol';
 import './DuelistKingConst.sol';
+import './interfaces/ITheDivine.sol';
 
 contract DuelistKingFairDistributor is DuelistKingConst, Ownable {
   // Registry contract
-  DuelistKingRegistry private registry;
+  DuelistKingRegistry private immutable registry;
+
+  // The Divine on Binance Smart Chain
+  ITheDivine private immutable TheDivine = ITheDivine(0xF52a83a3B7d918B66BD9ae117519ddC436A82031);
+
+  // Caompaign structure
   struct Campaign {
-    // Total number of loot box
-    uint64 totalSupply;
-    // Remaining supply
-    uint64 remainningSupply;
-    // Number of card in each box
-    uint64 totalCard;
+    // Remaining card
+    uint18 totalCards;
     // Deadline
-    uint64 deadline;
+    uint18 deadline;
     // Card distribution
     uint256[] distribution;
   }
 
+  // Buy order structure
+  struct BuyOrder {
+    uint256 campaign;
+    uint128 quanity;
+    uint128 memo;
+    address beneficial;
+  }
+
   mapping(uint256 => Campaign) private campaignStorage;
 
-  uint256 totalCampaign = 0;
+  mapping(uint256 => BuyOrder) private buyOrderStorage;
+
+  uint256 campaignIndex = 0;
+
+  uint256 buyOrderIndex = 0;
+
+  event BuyLootBoxes(uint256 indexed campaign, uint256 indexed order, uint256 indexed memo);
+  event OpenLootBoxes();
 
   modifier onlyRng() {
     // DuelistKingRng
     require(
-      msg.sender == registry.getAddres(DuelistKingConst.Rng),
+      msg.sender == registry.getAddress(DuelistKingConst.Rng),
       'FairDistributor: Only allow calls from Duelist King RNG'
+    );
+    _;
+  }
+
+  modifier onlyValidOrder(BuyOrder memory newBuyOrder) {
+    require(newBuyOrder.beneficial != address(0), 'FairDistributor: We do not accept zero address');
+    require(newBuyOrder.campaign == campaignIndex, 'FairDistributor: Wrong campaign id');
+    require(
+      newBuyOrder.quanity == 1 || newBuyOrder.quanity == 5 || newBuyOrder.quanity == 10,
+      'FairDistributor: Wrong quality of loot boxes'
     );
     _;
   }
@@ -42,12 +70,34 @@ contract DuelistKingFairDistributor is DuelistKingConst, Ownable {
   }
 
   function newCampaign(Campaign calldata campaign) external onlyOwner returns (uint256) {
-    totalCampaign += 1;
-    campaignStorage[totalCampaign] = campaign;
-    return totalCampaign;
+    campaignIndex += 1;
+    campaignStorage[campaignIndex] = campaign;
+    return campaignIndex;
   }
 
-  function issueNewCard(bytes32 randomValue) external onlyRng returns (bool) {
-    uint256 a = uint256(randomValue);
+  function buyLootBoxes(BuyOrder memory newBuyOrder) external onlyValidOrder(newBuyOrder) returns (uint256) {
+    buyOrderIndex += 1;
+    buyOrderStorage[buyOrderIndex] = newBuyOrder;
+    // Emit loot box buy
+    emit BuyLootBoxes(campaignIndex, buyOrderIndex, newBuyOrder.memo);
+    return buyOrderIndex;
+  }
+
+  function openLootBox(bytes32 randomValue) external onlyRng returns (bool) {
+    // We combine random value with the divine result to prevent manipulation
+    // https://github.com/chiro-hiro/thedivine
+    uint256 rnd = uint256(randomValue) ^ TheDivine.rand();
+    Campaign memory currentCampaign = campaignStorage[campaignIndex];
+    BuyOrder memory currentBuyOrder = buyOrderStorage[buyOrderIndex];
+    uint256 boughtCards = currentBuyOrder.quanity * 5;
+    uint256 luckyNumber = 0;
+    for (uint256 i = 0; i < boughtCards; i += 1) {
+      // Repeat on its selft
+      rnd = uint256(keccak256(abi.encodePacked(rnd)));
+      luckyNumber = rnd % currentCampaign.remainingCard;
+      for (uint256 j = 0; j < currentCampaign.distribution.length; j += 1) {
+        if(currentCampaign.distribution[j] )
+      }
+    }
   }
 }
