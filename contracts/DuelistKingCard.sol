80185ae93b3e0d0e74e9342b2141c468e2040789
Chiro Hiro <chiro8x@gmail.com>
2021-06-05 23:44:44 +0700

    update smart contract of Duelist King base on DKDAO

--- contracts/DuelistKingCard.sol
+++ contracts/DuelistKingCard.sol
@@ -3,17 +3,23 @@
 pragma solidity >=0.8.4 <0.9.0;
 pragma abicoder v2;
 
+/**
+ * Card of Duelist King
+ * Name: Card
+ * Domain: Duelist King
+ */
 library DuelistKingCard {
-  // We have 256 bits to store a Item id so we dicide to contain as much as posible data
-  // Application      64  bits    0-Duelist King Card
-  // Reversed         72  bits    Will be used in the future
-  // Edition:         8   bits    For now, 0-Standard edition 0xff-Creator edition
+  // We have 256 bits to store an item's id so we dicide to contain as much as posible data
+  // Application      64  bits    We can't control this, it will be assigned by DKDAO
+
+  // Edition:         16  bits    For now, 0-Standard edition 0xff-Creator edition
   // Generation:      16  bits    Generation of item, now it's Gen 0
-  // Rareness:        16  bits    0-C,1-U,2-R,3-SR,4-SSR,5-L
+  // Rareness:        16  bits    0-C, 1-U, 2-R, 3-SR, 4-SSR, 5-L
   // Type:            16  bits    0-Card, 1-Loot Box
-  // Id:              32  bits    Increasement value that unique for each item
-  // Serial:          32  bits    Increasement value that count the number of items
-  // |application|<--reversed-->|edition|generation|rareness|type|id|seiral|
+  // Id:              64  bits    Increasement value that unique for each item
+  // Serial:          64  bits    Increasement value that count the number of items
+  // 256         192         176             160            144         128       64            0 
+  //  |application|  edition  |  generation   |   rareness   |   type    |   id    |   seiral   |
   function set(
     uint256 value,
     uint256 shift,
@@ -38,58 +44,50 @@ library DuelistKingCard {
   }
 
   function setSerial(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 0, 0xffffffff, b);
+    return set(a, 0, 0xffffffffffffffff, b);
   }
 
   function getSerial(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 0, 0xffffffff);
+    return get(a, 0, 0xffffffffffffffff);
   }
 
   function setId(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 32, 0xffffffff, b);
+    return set(a, 64, 0xffffffffffffffff, b);
   }
 
   function getId(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 32, 0xffffffff);
+    return get(a, 64, 0xffffffffffffffff);
   }
 
   function setType(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 48, 0xffff, b);
+    return set(a, 128, 0xffff, b);
   }
 
   function getType(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 48, 0xffff);
+    return get(a, 128, 0xffff);
   }
 
   function setRareness(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 64, 0xffff, b);
+    return set(a, 144, 0xffff, b);
   }
 
   function getRareness(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 64, 0xffff);
+    return get(a, 144, 0xffff);
   }
 
   function setGeneration(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 80, 0xffff, b);
+    return set(a, 160, 0xffff, b);
   }
 
   function getGeneration(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 80, 0xffff);
+    return get(a, 160, 0xffff);
   }
 
   function setEdition(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 88, 0xff, b);
+    return set(a, 176, 0xffff, b);
   }
 
   function getEdition(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 88, 0xff);
-  }
-
-  function setApplication(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    return set(a, 192, 0xffffffffffffffff, b);
-  }
-
-  function getApplication(uint256 a) internal pure returns (uint256 c) {
-    return get(a, 192, 0xffffffffffffffff);
+    return get(a, 176, 0xffff);
   }
 }
--- contracts/DuelistKingFairDistributor.sol
+++ contracts/DuelistKingFairDistributor.sol
@@ -3,101 +3,196 @@
 pragma solidity >=0.8.4 <0.9.0;
 pragma abicoder v2;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-import './DuelistKingCard.sol';
-import './DuelistKingItem.sol';
-import './DuelistKingRegistry.sol';
-import './DuelistKingConst.sol';
+import 'dkdao/interfaces/IRNGConsumer.sol';
+import 'dkdao/libraries/User.sol';
 import './interfaces/ITheDivine.sol';
+import './DuelistKingCard.sol';
 
-contract DuelistKingFairDistributor is DuelistKingConst, Ownable {
-  // Registry contract
-  DuelistKingRegistry private immutable registry;
+/**
+ * Card distributor
+ * Name: Distributor
+ * Domain: Duelist King
+ */
+contract DuelistKingFairDistributor is User, IRNGConsumer {
+  // NFT by given domain
+  using DuelistKingCard for uint256;
 
-  // The Divine on Binance Smart Chain
-  ITheDivine private immutable TheDivine = ITheDivine(0xF52a83a3B7d918B66BD9ae117519ddC436A82031);
+  // Card id of unique design
+  uint256 private assignedCardId;
+
+  // Private random
+  uint256 private randomValue;
+
+  // Campaign index
+  uint256 campaignIndex;
+
+  // The Divine
+  ITheDivine private immutable theDivine;
 
   // Caompaign structure
   struct Campaign {
-    // Remaining card
-    uint18 totalCards;
+    // Total card
+    uint128 totalCard;
+    // Remaining card of current campaign
+    uint128 remainingCard;
     // Deadline
-    uint18 deadline;
+    uint64 deadline;
+    // Generation
+    uint64 generation;
+    // Start card Id
+    uint64 start;
+    // Number of rareness by distribution
+    // 0-C,1-U,2-R,3-SR,4-SSR,5-L
+    uint256[] rareness;
     // Card distribution
     uint256[] distribution;
+    // Last deployed cards
+    uint256[] deployed;
   }
 
-  // Buy order structure
-  struct BuyOrder {
-    uint256 campaign;
-    uint128 quanity;
-    uint128 memo;
-    address beneficial;
-  }
-
+  // Campaign storage
   mapping(uint256 => Campaign) private campaignStorage;
 
-  mapping(uint256 => BuyOrder) private buyOrderStorage;
+  // New campaign
+  event NewCampaign(uint256 indexed campaginId, uint256 indexed generation, uint128 totalCard);
+  // Found a card
+  event NewCard(uint256 indexed campaginId, address indexed owner, uint256 indexed card);
 
-  uint256 campaignIndex = 0;
+  constructor(
+    address _registry,
+    bytes32 _domain,
+    address divine
+  ) {
+    init(_registry, _domain);
+    theDivine = ITheDivine(divine);
+  }
 
-  uint256 buyOrderIndex = 0;
-
-  event BuyLootBoxes(uint256 indexed campaign, uint256 indexed order, uint256 indexed memo);
-  event OpenLootBoxes();
-
-  modifier onlyRng() {
-    // DuelistKingRng
+  // Create new campaign
+  function newCampaign(Campaign memory campaign) external onlyAllowSameDomain(bytes32('Oracle')) returns (uint256) {
+    require(campaign.totalCard == campaign.remainingCard, 'FairDistributor: Total number and remaining must equal');
     require(
-      msg.sender == registry.getAddress(DuelistKingConst.Rng),
-      'FairDistributor: Only allow calls from Duelist King RNG'
+      campaign.rareness.length == campaign.distribution.length &&
+        campaign.deployed.length == campaign.distribution.length,
+      'FairDistributor: Number of item should be the same'
     );
-    _;
-  }
-
-  modifier onlyValidOrder(BuyOrder memory newBuyOrder) {
-    require(newBuyOrder.beneficial != address(0), 'FairDistributor: We do not accept zero address');
-    require(newBuyOrder.campaign == campaignIndex, 'FairDistributor: Wrong campaign id');
-    require(
-      newBuyOrder.quanity == 1 || newBuyOrder.quanity == 5 || newBuyOrder.quanity == 10,
-      'FairDistributor: Wrong quality of loot boxes'
-    );
-    _;
-  }
-
-  constructor(address _registry) {
-    registry = DuelistKingRegistry(_registry);
-  }
-
-  function newCampaign(Campaign calldata campaign) external onlyOwner returns (uint256) {
+    // Overwrite start with number of unique design
+    // and then increase unique design to new card
+    // To make sure card id won't be duplicated
+    campaign.start = uint64(assignedCardId);
+    assignedCardId += campaign.distribution.length;
+    // Auto assign generation
     campaignIndex += 1;
+    campaign.generation = uint64(campaignIndex / 25);
     campaignStorage[campaignIndex] = campaign;
+    emit NewCampaign(campaignIndex, campaign.generation, campaign.totalCard);
     return campaignIndex;
   }
 
-  function buyLootBoxes(BuyOrder memory newBuyOrder) external onlyValidOrder(newBuyOrder) returns (uint256) {
-    buyOrderIndex += 1;
-    buyOrderStorage[buyOrderIndex] = newBuyOrder;
-    // Emit loot box buy
-    emit BuyLootBoxes(campaignIndex, buyOrderIndex, newBuyOrder.memo);
-    return buyOrderIndex;
-  }
-
-  function openLootBox(bytes32 randomValue) external onlyRng returns (bool) {
+  // Compute random value from RNG
+  function compute(bytes32 secret)
+    external
+    override
+    onlyAllowCrossDomain(bytes32('DKDAO Infrasctructure'), bytes32('RNG'))
+    returns (bool)
+  {
     // We combine random value with the divine result to prevent manipulation
     // https://github.com/chiro-hiro/thedivine
-    uint256 rnd = uint256(randomValue) ^ TheDivine.rand();
-    Campaign memory currentCampaign = campaignStorage[campaignIndex];
-    BuyOrder memory currentBuyOrder = buyOrderStorage[buyOrderIndex];
-    uint256 boughtCards = currentBuyOrder.quanity * 5;
-    uint256 luckyNumber = 0;
-    for (uint256 i = 0; i < boughtCards; i += 1) {
-      // Repeat on its selft
-      rnd = uint256(keccak256(abi.encodePacked(rnd)));
-      luckyNumber = rnd % currentCampaign.remainingCard;
-      for (uint256 j = 0; j < currentCampaign.distribution.length; j += 1) {
-        if(currentCampaign.distribution[j] )
+    // Combine secret with with thedivine salt
+    randomValue ^= uint256(secret) ^ theDivine.rand();
+    return true;
+  }
+
+  // Calculate rareness
+  function caculateRareness(
+    Campaign memory currentCampaign,
+    uint256 luckyNumber,
+    uint256 currentSeek
+  ) private pure returns (bool, uint256) {
+    uint256 start;
+    uint256 end;
+    for (uint256 i = 0; i < currentSeek; i += 1) {
+      start += currentCampaign.distribution[i];
+    }
+    end = start + currentCampaign.distribution[currentSeek];
+    if (luckyNumber >= start && luckyNumber < end) {
+      return (true, currentCampaign.rareness[currentSeek]);
+    }
+    // Otherwise return false
+    return (false, 0);
+  }
+
+  // Calcualte card
+  function caculateCard(Campaign memory currentCampaign, uint256 luckyNumber) private pure returns (uint256, uint256) {
+    bool success;
+    uint256 rareness;
+    uint256 cardId;
+    for (uint256 j = 0; j < currentCampaign.distribution.length; j += 1) {
+      (success, rareness) = caculateRareness(currentCampaign, luckyNumber, j);
+      if (success) {
+        // Increase serial number by one
+        cardId.setSerial(currentCampaign.deployed[j].getSerial() + 1);
+        // Calculate card
+        cardId.setId(currentCampaign.start + j);
+        cardId.setRareness(rareness);
+        cardId.setGeneration(currentCampaign.generation);
+        return (j, cardId);
       }
     }
+    return (0, 0);
+  }
+
+  // Open loot boxes
+  function openBox(uint256 numberOfBoxes, address buyer)
+    external
+    onlyAllowSameDomain(bytes32('Oracle'))
+    returns (bool)
+  {
+    // Make sure number of loot boxes won't be too munch
+    require(
+      numberOfBoxes == 1 || numberOfBoxes == 5 || numberOfBoxes == 10,
+      'FairDistributor: Number of loot box must be 1/5/10'
+    );
+
+    Campaign memory currentCampaign = campaignStorage[campaignIndex];
+
+    // Make sure card won't be sold after deadline
+    if (currentCampaign.deadline > 0) {
+      require(block.timestamp < currentCampaign.deadline, 'FairDistributor: The card sell was over');
+    }
+    if (
+      currentCampaign.deadline == 0 &&
+      currentCampaign.totalCard - currentCampaign.remainingCard >= (currentCampaign.totalCard / 4)
+    ) {
+      // If we reach soft cap we will close sell
+      currentCampaign.deadline = uint64(block.timestamp + 3 days);
+    }
+
+    uint256 rand = randomValue;
+    uint256 boughtCards = numberOfBoxes * 5;
+    uint256 luckyNumber;
+    uint256 card;
+    uint256 cardIndex;
+    for (uint256 i = 0; i < boughtCards; ) {
+      // Repeat hash on its selft
+      rand = uint256(keccak256(abi.encodePacked(rand)));
+      luckyNumber = rand % currentCampaign.remainingCard;
+      // Draw card by lucky number
+      (cardIndex, card) = caculateCard(currentCampaign, luckyNumber);
+      if (card > 0 && currentCampaign.distribution[cardIndex] > 0) {
+        emit NewCard(campaignIndex, buyer, card);
+        currentCampaign.remainingCard -= 1;
+        currentCampaign.distribution[cardIndex] -= 1;
+        i += 1;
+      }
+    }
+    campaignStorage[campaignIndex] = currentCampaign;
+    // Update the random value
+    randomValue = rand;
+    return true;
+  }
+
+  // Read campaign storage of given campaign index
+  function getCampaign(uint256 index) external view returns (Campaign memory) {
+    return campaignStorage[index];
   }
 }
